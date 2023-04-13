6daab033c5a04cae5e86c212c421d4f4d63a7396
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-13 21:58:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     axios
       .get("https://odd-teal-hummingbird-gear.cyclic.app/books")
       .then((response) => {
-        setQuestion(response);
+        setQuestion(response[0]);
       });
   };
 
