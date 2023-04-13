821d271fbc1adf0583f254f655047ed261928e77
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-13 10:22:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ function App() {
     axios
       .get("https://opentdb.com/api.php?amount=1&type=multiple")
       .then((response) => {
-        let returnQuestion = response.data.response_code;
-        setQuestion(returnQuestion);
+        setQuestion(response.data.response_code);
       });
   };
 
