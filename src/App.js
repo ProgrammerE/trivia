4f44bb5ac0f733fc51328e2f559794369615d8fb
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 09:25:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("https://odd-teal-hummingbird-gear.cyclic.app/books")
+    fetch("https://opentdb.com/api.php?amount=1&type=multiple")
       .then((res) => res.json())
       .then((data) => setMessage(data));
   }, []);
 
   return (
     <div className="App">
-      <h1>Get This</h1>
+      <h1>now avec trivia link</h1>
       <h1>{message}</h1>
     </div>
   );
