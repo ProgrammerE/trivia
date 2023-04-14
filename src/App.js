ba994e0f6a3b6efad45e41e286d71148477b0d7a
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 20:16:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function callTriviaApi() {
 }
 
 function callRaddyApi() {
-  fetch('https://odd-teal-hummingbird-gear.cyclic.app')
+  fetch('https://odd-teal-hummingbird-gear.cyclic.app/')
     .then(res => res)
     .then(res => alert(res))
     .then(alert('callRaddyApi'))
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello call 2014</h1>
+        <h1>hello call 2015</h1>
         <button onClick={callTriviaApi}>Call trivia API</button>
         <button onClick={callRaddyApi}>Call raddy API</button>
       </header>
