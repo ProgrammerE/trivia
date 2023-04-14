99f3b2f8e1e167249d04b9e9ad6873bdf71cc246
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 19:39:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { "" };
-    }
+function callApi() {
+  fetch('https://opentdb.com/api.php?amount=1&type=multiple')
+    .then(data => res.json())
+    .then(json => alert(JSON.stringify(json)))
+}
 
-    callAPI() {
-        fetch("https://opentdb.com/api.php?amount=1&type=multiple")
-            .then(res => res.text())
-            .then(res => this.setState({ res }))
-            .catch(err => err);
-    }
-
-    componentDidMount() {
-        this.callAPI();
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>Welcome to React</h1>
-                </header>
-                <p>{this.state}</p>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={callApi}>Call API</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
