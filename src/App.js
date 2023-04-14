4eea0ec0d2ed175dfdb3daec80b49c35b9f71158
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 19:50:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function callApi() {
-  fetch('https://odd-teal-hummingbird-gear.cyclic.app/books')
+  fetch('https://opentdb.com/api.php?amount=1&type=multiple')
     .then(res => res.text())
     .then(text => alert(text))
     //.then(json => alert(JSON.stringify(json)))
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello call raddy1949</h1>
+        <h1>hello call trivia1950</h1>
         <button onClick={callApi}>Call API</button>
       </header>
     </div>
