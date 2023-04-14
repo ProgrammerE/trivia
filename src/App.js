b6f5040c0db53eff43b3ee21150e8c4650427f27
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 19:53:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function callApi() {
   fetch('https://opentdb.com/api.php?amount=1&type=multiple')
-    .then(res => res.text())
+    .then(res => res)
     .then(text => alert(text))
     //.then(json => alert(JSON.stringify(json)))
 }
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello call trivia1950</h1>
+        <h1>hello call triviajustres1953</h1>
         <button onClick={callApi}>Call API</button>
       </header>
     </div>
