a5a431943bfce2c5f27c15b858d7259c716db202
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 20:00:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 
 function callApi() {
-  fetch('https://odd-teal-hummingbird-gear.cyclic.app/books', { method: 'GET' })
+  fetch('https://odd-teal-hummingbird-gear.cyclic.app/books')
     .then(res => res)
     .then(text => alert(text))
+    .then(alert('wtf')
     //.then(json => alert(JSON.stringify(json)))
 }
 
@@ -12,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello call raddyjustresmethodget1958</h1>
+        <h1>hello call raddyjustres2000</h1>
         <button onClick={callApi}>Call API</button>
       </header>
     </div>
