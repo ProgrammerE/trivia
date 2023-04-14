47c72f058ae96e5153c8a21459914f84212ef07c
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 21:31:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 
 function callTriviaApi() {
   fetch('https://opentdb.com/api.php?amount=1&type=multiple')
-    .then(res => res.text())
-    .then(res => alert(res))
+    .then(res => res)
+    .then(res => alert(res.text()))
     .then(alert('callTriviaApi'))
     //.then(json => alert(JSON.stringify(json)))
 }
 
 function callRaddyApi() {
   fetch('https://odd-teal-hummingbird-gear.cyclic.app/books', { method: 'GET', mode: 'no-cors' })
-    .then(res => res.text())
+    .then(res => res)
     .then(res => alert(res))
     .then(alert('callRaddyApi'))
   console.log('hello')
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello call 2128</h1>
+        <h1>hello call 2131</h1>
         <button onClick={callTriviaApi}>Call trivia API</button>
         <button onClick={callRaddyApi}>Call raddy API</button>
       </header>
