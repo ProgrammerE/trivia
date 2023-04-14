b56eb54c710439f448b5d2e7aa5ff8eca50abff3
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 21:17:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function callTriviaApi() {
 }
 
 function callRaddyApi() {
-  fetch('https://odd-teal-hummingbird-gear.cyclic.app/books', { method: 'GET', mode: 'cors', headers: {'Access-Control-Allow-Origin': 'https://odd-teal-hummingbird-gear.cyclic.app/books'} })
+  fetch('https://odd-teal-hummingbird-gear.cyclic.app/books', { method: 'GET', mode: 'no-cors'} })
     .then(res => res)
     .then(res => alert(res))
     .then(alert('callRaddyApi'))
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello call 2115</h1>
+        <h1>hello call 2117</h1>
         <button onClick={callTriviaApi}>Call trivia API</button>
         <button onClick={callRaddyApi}>Call raddy API</button>
       </header>
