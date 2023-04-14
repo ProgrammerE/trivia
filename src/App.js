a0af3b72ddf6f8a3384a6289e60d9361a495a1b7
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 20:08:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function callApi() {
+function callTriviaApi() {
   fetch('https://opentdb.com/api.php?amount=1&type=multiple')
     .then(res => res)
     .then(res => alert(res))
-    .then(alert('wtf'))
+    .then(alert('callTriviaApi'))
+    //.then(json => alert(JSON.stringify(json)))
+}
+
+function callRaddyApi() {
+  fetch('https://odd-teal-hummingbird-gear.cyclic.app/books')
+    .then(res => res)
+    .then(res => alert(res))
+    .then(alert('callRaddyApi'))
     //.then(json => alert(JSON.stringify(json)))
 }
 
@@ -13,8 +21,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello call triviaalertres2003</h1>
-        <button onClick={callApi}>Call API</button>
+        <h1>hello call 2008</h1>
+        <button onClick={callTriviaApi}>Call trivia API</button>
+        <button onClick={callRaddyApi}>Call trivia API</button>
       </header>
     </div>
   );
