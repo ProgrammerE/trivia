3a723b007ab303969e729779391b3dccf193c292
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 21:22:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function callTriviaApi() {
 
 function callRaddyApi() {
   fetch('https://odd-teal-hummingbird-gear.cyclic.app/books', { method: 'GET', mode: 'no-cors' })
-    .then(res => res)
+    .then(res => res.text())
     .then(res => alert(res))
     .then(alert('callRaddyApi'))
   console.log('hello')
@@ -22,12 +22,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello call 2117</h1>
+        <h1>hello call 2121</h1>
         <button onClick={callTriviaApi}>Call trivia API</button>
         <button onClick={callRaddyApi}>Call raddy API</button>
       </header>
 
-      <p>delivering headers</p>
+      <p>text</p>
 
     </div>
   );
