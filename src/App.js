daf2fa50adde50016903b5e033d0bf009ec5acc5
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-14 19:45:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello</h1>
+        <h1>hello call raddy</h1>
         <button onClick={callApi}>Call API</button>
       </header>
     </div>
